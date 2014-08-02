b46b3dea693178b3032604473961ca8841c6a1c2
Roger Labbe <rlabbejr@gmail.com>
2014-08-02 11:02:07 -0700

    Implemented Extended Kalman Filter
    NOT TESTED!!! Implemented the Extended Kalman filter. Also changed
    some of the documentation for the KalmanFilter class.

--- kalman/kalman_filter.py
+++ kalman/kalman_filter.py
@@ -49,12 +49,15 @@ class KalmanFilter(object):
         self.x = zeros((dim_x,1)) # state
         self.P = eye(dim_x)       # uncertainty covariance
         self.Q = eye(dim_x)       # process uncertainty
-        self.u = 0                # control input vector
-        self.B = 0
+        self.B = 0                # control transistion matrx
         self.F = 0                # state transition matrix
         self.H = 0                # Measurement function
         self.R = eye(dim_z)       # state uncertainty
-        self.K = 0                # kalman gain
+
+        # gain and residual are computed during the innovation step. We
+        # save them so that in case you want to inspect them for various
+        # purposes
+        self.K = 0 # kalman gain
         self.residual = zeros((dim_z, 1))
 
         # identity matrix. Do not alter this.
@@ -66,13 +69,14 @@ class KalmanFilter(object):
         Add a new measurement (Z) to the kalman filter. If Z is None, nothing
         is changed.
 
-        Optionally provide R to override the measurement noise for this
-        one call, otherwise  self.R will be used.
+        Parameters
+        ----------
+        Z : np.array
+            measurement for this update.
 
-        self.residual, self.S, and self.K are stored in case you want to
-        inspect these variables. Strictly speaking they are not part of the
-        output of the Kalman filter, however it is often useful to know
-        what these values are in various scenarios.
+        R : np.array, scalar, or None
+            Optionally provide R to override the measurement noise for this
+            one call, otherwise  self.R will be used.
         """
 
         if Z is None:
@@ -112,11 +116,17 @@ class KalmanFilter(object):
         self.K = K
 
 
-    def predict(self, ekf_x=None):
-        """ Predict next position."""
+    def predict(self, u=0):
+        """ Predict next position.
+        Parameters
+        ----------
+        u : np.array
+            Optional control vector. If non-zero, it is multiplied by B
+            to create the control input into the system.
+        """
 
         # x = Fx + Bu
-        self.x = dot(self.F, self.x) + dot(self.B, self.u)
+        self.x = dot(self.F, self.x) + dot(self.B, u)
 
         # P = FPF' + Q
         self.P = dot3(self.F, self.P, self.F.T) + self.Q
@@ -132,13 +142,11 @@ class KalmanFilter(object):
             measurements must be represented by 'None'.
 
         Rs : list-like, optional
-
             optional list of values to use for the measurement error
             covariance; a value of None in any position will cause the filter
             to use `self.R` for that time step.
 
         update_first : bool, optional,
-
             controls whether the order of operations is update followed by
             predict, or predict followed by update. Default is predict->update.
 
@@ -181,28 +189,30 @@ class KalmanFilter(object):
         return (means, covariances)
 
 
-    def get_prediction(self):
-        """ Predicts the next state of the filter and returns it.
+    def get_prediction(self, u=0):
+        """ Predicts the next state of the filter and returns it. Does not
+        alter the state of the filter.
 
-        DOES NOT ALTER THE STATE OF THE FILTER.
+        Parameters
+        ----------
+        u : np.array
+            optional control input
 
         Returns
         -------
         (x, P)
-
-        State vector and covariance array of the prediction.
+            State vector and covariance array of the prediction.
         """
 
-        x = dot(self.F, self.x) + dot(self.B, self.u)
+        x = dot(self.F, self.x) + dot(self.B, u)
         P = dot3(self.F, self.P, self.F.T) + self.Q
         return (x, P)
 
 
     def residual_of(self, z):
-        """ returns the residual for the given measurement. Does not alter
+        """ returns the residual for the given measurement (z). Does not alter
         the state of the filter.
         """
-
         return z - dot(self.H, self.x)
 
 
@@ -220,3 +230,133 @@ class KalmanFilter(object):
             measurement corresponding to the given state
         """
         return dot(self.H, x)
+
+
+
+class ExtendedKalmanFilter(object):
+
+    def __init__(self, dim_x, dim_z):
+        """ Extended Kalman filter. You are responsible for setting the
+        various state variables to reasonable values; the defaults below will
+        not give you a functional filter.
+
+        Parameters
+        ----------
+        dim_x : int
+            Number of state variables for the Kalman filter. For example, if
+            you are tracking the position and velocity of an object in two
+            dimensions, dim_x would be 4.
+
+            This is used to set the default size of P, Q, and u
+
+        dim_z : int
+            Number of of measurement inputs. For example, if the sensor
+            provides you with position in (x,y), dim_z would be 2.
+        """
+
+        self.dim_x = dim_x
+        self.dim_z = dim_z
+
+        self.x = zeros((dim_x,1)) # state
+        self.P = eye(dim_x)       # uncertainty covariance
+        self.B = 0                # control transition matrix
+        self.F = 0                # state transition matrix
+        self.R = eye(dim_z)       # state uncertainty
+        self.Q = eye(dim_x)       # process uncertainty
+        self.residual = zeros((dim_z, 1))
+
+        # identity matrix. Do not alter this.
+        self._I = np.eye(dim_x)
+
+
+    def predict_update(self, z, HJabobian, Hx, u=0):
+        """ Performs the predict/update innovation of the extended Kalman
+        filter.
+
+        Parameters
+        ----------
+        z : np.array
+            measurement for this step.
+            If `None`, only predict step is perfomed.
+
+        HJacobian : function
+           function which computes the Jacobian of the H matrix (measurement
+           function). Takes state variable (self.x) as input, returns H.
+
+
+        Hx : function
+            function which takes a state variable and returns the measurement
+            that would correspond to that state.
+
+        u : np.array or scalar
+            optional control vector input to the filter.
+        """
+
+        F = self.F
+        P = self.P
+        Q = self.Q
+        R = self.R
+        B = self.B
+        x = self.x
+
+        H = HJabobian(x)
+
+        # predict step
+        x = dot(F, x) + dot(B, u)
+        P = dot3(F, P, F.T) + Q
+
+        # update step
+        S = dot3(H, P, H.T) + R
+        K = dot3(P, H.T, linalg.inv (S))
+
+        self.x = x + dot(K, (z - Hx(x)))
+
+        I_KH = self._I - dot(K, H)
+        self.P = dot3(I_KH, P, I_KH.T) + dot3(K, R, K.T)
+
+
+    def update(self, z, HJabobian, Hx):
+        """ Performs the update innovation of the extended Kalman filter.
+
+        Parameters
+        ----------
+        z : np.array
+            measurement for this step.
+            If `None`, only predict step is perfomed.
+
+        HJacobian : function
+           function which computes the Jacobian of the H matrix (measurement
+           function). Takes state variable (self.x) as input, returns H.
+
+
+        Hx : function
+            function which takes a state variable and returns the measurement
+            that would correspond to that state.
+        """
+
+        P = self.P
+        R = self.R
+        x = self.x
+
+        H = HJabobian(x)
+
+        S = dot3(H, P, H.T) + R
+        K = dot3(P, H.T, linalg.inv (S))
+
+        self.x = x + dot(K, (z - Hx(x)))
+
+        I_KH = self._I - dot(K, H)
+        self.P = dot3(I_KH, P, I_KH.T) + dot3(K, R, K.T)
+
+
+    def predict(self, u=0):
+        """ Predict next position.
+        Parameters
+        ----------
+        u : np.array
+            Optional control vector. If non-zero, it is multiplied by B
+            to create the control input into the system.
+        """
+
+        self.x = dot(self.F, self.x) + dot(self.B, u)
+        self.P = dot3(self.F, self.P, self.F.T) + self.Q
