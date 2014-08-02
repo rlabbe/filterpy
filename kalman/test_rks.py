e408e8f857730edd2fa7e5218941723d52f540e0
Roger Labbe <rlabbejr@gmail.com>
2014-08-02 10:16:03 -0700

    Implented RKS Smoother. Not sure on correctness yet.

--- kalman/test_rks.py
+++ kalman/test_rks.py
@@ -17,7 +17,8 @@ from filterpy.kalman import KalmanFilter, RKSSmoother
 
 if __name__ == '__main__':
 
-    f = KalmanFilter (dim_x=2, dim_z=1)
+    f = RKSSmoother (dim_x=2, dim_z=1)
+    fk = KalmanFilter(dim_x=2, dim_z=1)
 
     f.x = np.array([[2.],
                     [0.]])        # initial state (location and velocity)
@@ -27,26 +28,36 @@ if __name__ == '__main__':
 
     f.H = np.array([[1.,0.]])     # Measurement function
     f.P *= .01                     # covariance matrix
-    f.R *= 5                      # state uncertainty
-    f.Q *= 0.0001                 # process uncertainty
+    f.R *= 2                      # state uncertainty
+    f.Q *= 0.01                 # process uncertainty
 
-    f.P[0,0] = 1
+    f.P[0,0] = .5
+    
+    fk.x = f.x.copy()
+    fk.F = f.F.copy()
+    fk.H = f.H.copy()
+    fk.P = f.P.copy()
+    fk.R = f.R.copy()
+    fk.Q = f.Q.copy()
 
-    zs = [t + random.randn()*20 for t in range (100)]
-    m,c = f.batch_filter(zs)
 
-    smoother = RKSSmoother()
-    m2, c2, d = smoother.smooth(m, c, f.F, f.Q)
-    for i in range(0):
-        m2[i]=m2[-1]
+    zs = [t + random.randn()*2 for t in range (40)]
+    
+    m = f.smooth(zs, 3)
+    ms = [x[0,0] for x in m]
+    
+    mu, cov = fk.batch_filter (zs)
+    mus = [x[0,0] for x in mu]
+        
+
 
     # plot data
     p1, = plt.plot(zs,'r', alpha=0.5)
-    p2, = plt.plot (m[:,0],'b')
-    p4, = plt.plot(m2[:,0], 'm')
-    p3, = plt.plot ([0,100],[0,100], 'g') # perfect result
+    p2, = plt.plot (ms,c='b')
+    p3, = plt.plot (mus,c='r')
+    p4, = plt.plot ([0,len(zs)],[0,len(zs)], 'g') # perfect result
     plt.legend([p1,p2, p3, p4],
-               ["noisy measurement", "KF output", "ideal", "smooth"], 4)
+               ["measurement", "RKS", "KF output", "ideal"], 4)
 
 
     plt.show()
