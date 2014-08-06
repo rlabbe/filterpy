26ec249b6afdd915b5fb0541250ac2f69bea8514
Roger Labbe <rlabbejr@gmail.com>
2014-08-05 20:59:15 -0700

    Added copyright notice at top of file.

--- memory/test_fm.py
+++ memory/test_fm.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Tue Aug  5 20:47:55 2014
+"""Copyright 2014 Roger R Labbe Jr.
 
-@author: rlabbe
+filterpy library.
+http:\\github.com\rlabbe\filterpy
+
+This is licensed under an MIT license. See the readme.MD file
+for more information.
 """
 
 from filterpy.memory import FadingMemoryFilter
