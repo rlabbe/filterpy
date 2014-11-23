08f94d1fd14f58a673e6cda97c51239ce311902b
Roger Labbe <rlabbejr@gmail.com>
2014-11-22 18:18:15 -0800

    Trying to build Sphinx for ReadTheDocs.
    Mocking numpy et., al.

--- docs/conf.py
+++ docs/conf.py
@@ -16,6 +16,12 @@
 import sys
 import os
 
+import mock
+ 
+MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'matplotlib.pyplot', 'scipy.interpolate']
+for mod_name in MOCK_MODULES:
+    sys.modules[mod_name] = mock.Mock()
+
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
