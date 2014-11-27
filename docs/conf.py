2c918fb4bdd2bd30b5d4e3fdce899bd4d53a95f2
Roger Labbe <rlabbejr@gmail.com>
2014-11-27 00:35:55 -0800

    Removed napolean - ReadTheDocs can't import it.

--- docs/conf.py
+++ docs/conf.py
@@ -49,8 +49,7 @@ extensions = [
     'sphinx.ext.intersphinx',
     'sphinx.ext.mathjax',
     'sphinx.ext.viewcode',
-    'sphinx.ext.autodoc', 
-    'sphinxcontrib.napoleon'
+    'sphinx.ext.autodoc'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
