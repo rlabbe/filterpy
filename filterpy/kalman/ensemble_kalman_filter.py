Mean computation bug fix.
Only the covariances should use N-1 to normalize; the means should
use N. To be honest I do not yet understand why N-1 should be used.
Brown uses N, but the filter does not work with that. Crassidis uses
N-1, and that works.

filterpy/kalman/ensemble_kalman_filter.py
@@ -16,6 +16,7 @@ import numpy as np
 from scipy.linalg import inv
 from numpy import dot, zeros, eye, outer
 from numpy.random import multivariate_normal
+from filterpy.common import dot3
 
 
 class EnsembleKalmanFilter(object):
@@ -120,13 +121,13 @@ class EnsembleKalmanFilter(object):
             s = sigma - z_k
             P_zz += outer(s, s)
 
-        P_zz = (P_zz / (N)) + R# - outer(z_k, z_k) + R
+        P_zz = (P_zz / (N-1)) + R
 
         P_xz = 0
         for i in range(N):
             P_xz += outer(self.sigmas[i] - self.x, sigmas_h[i] - z_k)
 
-        P_xz /= (N)
+        P_xz /= (N-1)
 
         K = dot(P_xz, inv(P_zz))
 
@@ -137,33 +138,26 @@ class EnsembleKalmanFilter(object):
         for i in range(N):
             self.sigmas[i] += dot(K, z + e_r[i] - sigmas_h[i])
 
-        #self.sigmas += dot(K, z - z_k)
+        self.x = np.mean(self.sigmas, axis=0)
+        self.P = self.P - dot3(K, P_zz, K.T)
 
-        self.x = np.sum(self.sigmas, axis=0) / (N)
-        #self.P = self.P - dot3(K, P_zz, K.T)
+        print(self.P)
 
 
     def predict(self):
         """ Predict next position. """
 
-        x = 0
-
-        e = multivariate_normal(self.mean, self.Q, self.N)
+        N= self.N
         for i, s in enumerate(self.sigmas):
             self.sigmas[i] = self.fx(s, self.dt)
 
-        self.sigmas += e
+        e = multivariate_normal(self.mean, self.Q, N)
 
-        self.x = np.sum(self.sigmas, axis=0) / (self.N-1)
+        self.x = np.mean(self.sigmas + e, axis=0)
 
+        P = 0
+        for s in self.sigmas:
+            sx = s - self.x
+            P += outer(sx, sx)
 
-def f1():
-    su = 0
-    for i in range(len(sigmas)):
-        su += np.dot (sigmas[i], 3)
-
-def f2():
-    su = 0
-    for i,s in enumerate(sigmas):
-        su += np.dot (s, 3)
-
+        self.P = (P) / (N-1)

filterpy/kalman/tests/test_enkf.py
@@ -11,46 +11,66 @@ import matplotlib.pyplot as plt
 from filterpy.kalman import EnsembleKalmanFilter as EnKF
 from filterpy.common import Q_discrete_white_noise
 
-#def test_noisy_1d():
 
-def hx(x):
-    return np.array([x[0]])
+DO_PLOT = False
 
-F = np.array([[1., 1.],[0., 1.]])
-def fx(x, dt):
-    return np.array([x[0]+x[1], x[1]])
+def test_1d_const_vel():
 
-x = np.array([0., 1.])
-P = np.eye(2)* 100.
-f = EnKF(x=x, P=P, dim_z=1, dt=1., N=8, hx=hx, fx=fx)
+    def hx(x):
+        return np.array([x[0]])
+
+    F = np.array([[1., 1.],[0., 1.]])
+    def fx(x, dt):
+        return np.array([x[0]+x[1], x[1]])
+
+    x = np.array([0., 1.])
+    P = np.eye(2)* 100.
+    f = EnKF(x=x, P=P, dim_z=1, dt=1., N=8, hx=hx, fx=fx)
 
 
-std_noise = 3.
+    std_noise = 3.
 
-f.R *= std_noise**2
-f.Q = Q_discrete_white_noise(2, 1., 0.01)
+    f.R *= std_noise**2
+    f.Q = Q_discrete_white_noise(2, 1., .001)
 
-f.initialize (f.x, f.P)
-print(np.mean(f.sigmas,axis=0))
+    f.initialize (f.x, f.P)
 
-measurements = []
-results = []
+    measurements = []
+    results = []
+    ps = []
 
-zs = []
-for t in range (1,100):
-    # create measurement = t plus white noise
-    z = t + randn()*std_noise
-    zs.append(z)
+    zs = []
+    for t in range (0,100):
+        # create measurement = t plus white noise
+        z = t + randn()*std_noise
+        zs.append(z)
 
-    # perform kalman filtering
-    f.predict()
-    f.update(np.asarray([z]))
+        f.predict()
+        f.update(np.asarray([z]))
 
-    # save data
-    results.append (f.x[0])
-    measurements.append(z)
+        # save data
+        results.append (f.x[0])
+        measurements.append(z)
+        ps.append(f.P[0,0]**.5)
+        print()
 
-plt.plot(results)
-plt.plot(measurements, c='r')
+    results = np.asarray(results)
+    ps = np.asarray(ps)
+
+    if DO_PLOT:
+        plt.plot(results, label='EnKF')
+        plt.plot(measurements, c='r', label='z')
+        plt.plot (results-ps, c='k',linestyle='--')
+        plt.plot(results+ps, c='k', linestyle='--')
+        plt.legend(loc='best')
+        #print(ps)
+
+
+
+if __name__ == '__main__':
+    DO_PLOT = True
+
+    test_1d_const_vel()
+
 
 #test_noisy_1d()