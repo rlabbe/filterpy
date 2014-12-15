44d6749145ca95c559e46901dde7cd1c8a84cbcf
Roger Labbe <rlabbejr@gmail.com>
2014-12-15 13:40:39 -0800

    Fixed adding license and changelog to distribution.
    They were not being automaticallh included in the package
    like I expected to happen.

--- setup.py
+++ setup.py
@@ -78,9 +78,9 @@ setup(
     # If there are data files included in your packages that need to be
     # installed, specify them here.  If using Python 2.6 or less, then these
     # have to be included in MANIFEST.in as well.
-    #package_data={
-    #    'filterpy': ['README.rst'],
-    #},
+    package_data={
+        'filterpy': ['README.rst', 'changelog.txt', 'LICENSE.txt'],
+    },
 
     # Although 'package_data' is the preferred approach, in some case you may
     # need to place data files outside of your packages.
