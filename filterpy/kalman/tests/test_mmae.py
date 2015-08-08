e2e167c83dcea6a766fcbe44b4749babab86e607
Roger Labbe <rlabbejr@gmail.com>
2015-08-08 09:50:10 -0700

    Removed unused test.

--- filterpy/kalman/tests/test_mmae.py
+++ filterpy/kalman/tests/test_mmae.py
@@ -148,38 +148,6 @@ def generate_data(steady_count, noise_factor):
 
 
 
-def test_MMAE():
-    dt = 0.1
-    pos, zs = generate_data(120, noise_factor=0.2)
-    z_xs = zs[:, 0]
-    t = np.arange(0, len(z_xs) * dt, dt)
-
-    dt = 0.1
-    filters = [make_cv_filter(dt, noise_factor=0.2),
-               make_ca_filter(dt, noise_factor=0.2)]
-    H_cv = np.array([[1., 0, 0],
-                     [0., 1, 0]])
-
-    H_ca = np.array([[1., 0., 0.],
-                     [0., 1., 0.],
-                     [0., 0., 1.]])
-
-    bank = MMAEFilterBank(filters, (0.5, 0.5), dim_x=3, H=(H_cv, H_ca))
-
-    xs, probs = [], []
-    for z in z_xs:
-        bank.predict()
-        bank.update(z)
-        xs.append(bank.x[0])
-        #print('p', bank.p)
-        probs.append(bank.p[0] / bank.p[1])
-
-    if DO_PLOT:
-        plt.subplot(121)
-        plt.plot(xs)
-        plt.subplot(122)
-        plt.plot(probs)
-        plt.title('probability ratio p(cv)/p(ca)')
 
 
 def test_MMAE2():
