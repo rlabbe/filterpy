71590d0d212677d57787015b40b3ed910088e78e
Roger Labbe <rlabbejr@gmail.com>
2018-02-01 08:34:49 -0800

    Improved tests, creating version 1.2.0

--- filterpy/common/helpers.py
+++ filterpy/common/helpers.py
@@ -15,20 +15,6 @@ This is licensed under an MIT license. See the readme.MD file
 for more information.
 """
 
-import numpy as np
-from numpy.linalg import inv
-
-
-def _validate_vector(u, dtype=None):
-    # this is taken from scipy.spatial.distance. Internal function, so
-    # redefining here.
-
-    u = np.asarray(u, dtype=dtype).squeeze()
-    # Ensure values such as u=1 and u=[1] still return 1-D arrays.
-    u = np.atleast_1d(u)
-    if u.ndim > 1:
-        raise ValueError("Input vector should be 1-D.")
-    return u
 
 
 def runge_kutta4(y, x, dx, f):
@@ -56,47 +42,4 @@ def runge_kutta4(y, x, dx, f):
 
     return y + (k1 + 2*k2 + 2*k3 + k4) / 6.
 
-
-def mahalanobis(x, mean, cov):
-    """ Computes the Mahalanobis distance between the state vector x from the
-    Gaussian  `mean` with covariance `cov`.
-
-
-    Parameters
-    ----------
-    x : (N,) array_like
-        Input state vector
-    mean : (N,) array_like
-        mean of multivariate Gaussian
-    cov : ndarray
-        covariance of the multivariate Gaussian
-
-
-    Returns
-    -------
-    mahalanobis : double
-        The Mahalanobis distance between vectors `x` and `mean`
-
-
-    Examples
-    --------
-    >>> mahalanobis(x=3., mean=3.5, cov=4.**2) # univariate case
-    0.125
-    >>> mahalanobis([1., 2], [1.1, 3.5], [[1., .1],[.1, 13]])
-    0.42533327058913922
-    """
-
-    x = _validate_vector(x)
-    mean = _validate_vector(mean)
-
-    if x.shape != mean.shape:
-        raise ValueError("length of input vectors must be the same")
-
-    y = x - mean
-    S = np.atleast_2d(cov)
-
-    # residual y is now 1D, so no need to transpose, and y @S^ @ y is a scalar
-    # so no array indexing required to get result
-    dist = np.dot(y, inv(S)).dot(y)
-    return np.sqrt(dist)
 
--- filterpy/stats/tests/test_stats.py
+++ filterpy/stats/tests/test_stats.py
@@ -14,9 +14,72 @@ This is licensed under an MIT license. See the readme.MD file
 for more information.
 """
 
-from filterpy.stats import norm_cdf, multivariate_gaussian, logpdf
-import numpy as np
+from __future__ import division
+
 from math import exp
+import numpy as np
+from numpy.linalg import inv
+from scipy.spatial.distance import mahalanobis as scipy_mahalanobis
+from filterpy.stats import norm_cdf, multivariate_gaussian, logpdf, mahalanobis
+
+
+ITERS = 10000
+
+def test_mahalanobis():
+    global a, b, S
+    # int test
+    a, b, S = 3, 1, 2
+    assert abs(mahalanobis(a, b, S) - scipy_mahalanobis(a, b, 1/S)) < 1.e-12
+
+
+    # int list
+    assert abs(mahalanobis([a], [b], [S]) - scipy_mahalanobis(a, b, 1/S)) < 1.e-12
+    assert abs(mahalanobis([a], b, S) - scipy_mahalanobis(a, b, 1/S)) < 1.e-12
+
+
+    # float
+    a, b, S = 3.123, 3.235235, .01234
+    assert abs(mahalanobis(a, b, S) - scipy_mahalanobis(a, b, 1/S)) < 1.e-12
+    assert abs(mahalanobis([a], [b], [S]) - scipy_mahalanobis(a, b, 1/S)) < 1.e-12
+    assert abs(mahalanobis([a], b, S) - scipy_mahalanobis(a, b, 1/S)) < 1.e-12
+
+    #float array
+    assert abs(mahalanobis(np.array([a]), b, S) - scipy_mahalanobis(a, b, 1/S)) < 1.e-12
+
+    #1d array
+    a = np.array([1., 2.])
+    b = np.array([1.4, 1.2])
+    S = np.array([[1., 2.], [2., 4.001]])
+
+    assert abs(mahalanobis(a, b, S) - scipy_mahalanobis(a, b, inv(S))) < 1.e-12
+
+    #2d array
+    a = np.array([[1., 2.]])
+    b = np.array([[1.4, 1.2]])
+    S = np.array([[1., 2.], [2., 4.001]])
+
+    assert abs(mahalanobis(a, b, S) - scipy_mahalanobis(a, b, inv(S))) < 1.e-12
+    assert abs(mahalanobis(a.T, b, S) - scipy_mahalanobis(a, b, inv(S))) < 1.e-12
+    assert abs(mahalanobis(a, b.T, S) - scipy_mahalanobis(a, b, inv(S))) < 1.e-12
+    assert abs(mahalanobis(a.T, b.T, S) - scipy_mahalanobis(a, b, inv(S))) < 1.e-12
+
+    try:
+        # mismatched shapes
+        mahalanobis([1], b, S)
+        assert "didn't catch vectors of different lengths"
+    except ValueError:
+        pass
+    except:
+        assert "raised exception other than ValueError"
+
+    # okay, now check for numerical accuracy
+    for _ in range(ITERS):
+        N = np.random.randint(1, 20)
+        a = np.random.randn(N)
+        b = np.random.randn(N)
+        S = np.random.randn(N, N)
+        S = np.dot(S, S.T) #ensure positive semi-definite
+        assert abs(mahalanobis(a, b, S) - scipy_mahalanobis(a, b, inv(S))) < 1.e-12
 
 
 def test_multivariate_gaussian():
