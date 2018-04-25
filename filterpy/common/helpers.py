690f7ace90284e987923039fb9426429adf37956
Roger Labbe <rlabbejr@gmail.com>
2018-04-25 08:14:21 -0700

    Added pretty print function to print NumPy arrays.
    Prints NumPy arrays with optional assignment; to be used
    by the various classes in FilterPy to implement __repr__
    in a pleasing way.

--- filterpy/common/helpers.py
+++ filterpy/common/helpers.py
@@ -43,3 +43,75 @@ def runge_kutta4(y, x, dx, f):
     return y + (k1 + 2*k2 + 2*k3 + k4) / 6.
 
 
+
+
+
+def pretty_str(label, arr, transpose=True):
+
+    def is_col(a):
+        try:
+            return a.shape[1] == 1
+        except:
+            return False
+    """
+    Generates a pretty printed NumPy array with an assignment. Optionally
+    transposes column vectors so they are drawn on one line. Strictly speaking
+    arr can be any time convertible by `str(arr)`, but the output may not
+    be what you want if the type of the variable is not a scalar or an
+    ndarray.
+
+    Examples
+    --------
+    >>> pprint('cov', np.array([[4., .1], [.1, 5]]))
+    cov = [[4.  0.1]
+           [0.1 5. ]]
+
+    >>> print(pretty_str('x', np.array([[1], [2], [3]])))
+    x = [[1 2 3]].T
+    """
+
+    transposed = False
+    if is_col(arr):
+        arr = arr.T
+        transposed = True
+
+    rows = str(arr).split('\n')
+
+    if len(rows) == 0:
+        return ''
+
+    if label is None:
+        label = ''
+
+    if len(label) > 0:
+        label += ' = '
+
+    s = label + rows[0]
+    if transposed:
+        s += '.T'
+        return s
+
+    pad = ' ' * len(label)
+
+    for line in rows[1:]:
+        s = s + '\n' + pad + line
+
+    return s
+
+def pprint(label, arr, transpose=True, **kwargs):
+    """ pretty prints an NumPy array using the function pretty_str. Keyword
+    arguments are passed to the print() function.
+
+    See Also
+    --------
+    pretty_str
+
+    Examples
+    --------
+    >>> pprint('cov', np.array([[4., .1], [.1, 5]]))
+    cov = [[4.  0.1]
+           [0.1 5. ]]
+    """
+
+
+    print(pretty_str(label, arr, transpose), **kwargs)
