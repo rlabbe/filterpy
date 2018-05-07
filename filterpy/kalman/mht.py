e3980d4d4925561b7cf8bb09c5548a333f59e3ac
Roger Labbe <rlabbejr@gmail.com>
2018-05-06 18:06:49 -0700

    Working, split as separate node and tree
    Not sure I like this design, but it does make it easy to have
    separate set of all nodes, and all leaves. Not sure I see the
    need for the former, but will need the latter to efficiently
    add measurements.

--- filterpy/kalman/mht.py
+++ filterpy/kalman/mht.py
@@ -5,17 +5,24 @@ Created on Sun May  6 08:58:16 2018
 @author: rlabbe
 """
 
-import copy
 
 
 class Node(object):
     def __init__(self, kf):
 
-        self.kf = copy.deepcopy(kf)
+        self.kf = kf
+        self.score = 1.0
+
+        self.clear_ref()
+
+
+    def clear_ref(self):
+        """ Empty out parent and children, and set depth to one. """
+
         self.parent = None  # if None, I'm the root of the tree!
         self.children = set()
         self.depth = 1
-
+        self.score = 1.0
 
     def is_root(self):
         return self.parent is None
@@ -25,22 +32,44 @@ class Node(object):
         return len(self.children) == 0
 
 
-    '''def depth(self):
-        count = 0
-        p = self
-        while p is not None:
-            p = p.parent
-            count += 1
-        return count'''
+    def delete_children(self):
+        self.children = set()
+
+
+    def branch(self):
+        """
+        Generate a list of all of the nodes up to the root,
+        ordered from the head to this node
+
+        node_id will typically be a leaf node, but it doesn't have to be.
+        """
+
+        nodes = [self]
+
+        node = self.parent
+        while node is not None:
+            nodes.append(node)
+            node = node.parent
+
+        return list(reversed(nodes))
+
 
     def __repr__(self):
         return 'Node: ' + hex(id(self)) + ' ' + str(self.kf)
 
 
+# broken into separate tree and nodes so we can keep a master list
+# of nodes and leaves. We need to traverse leaves to add measurements
+# not sure if we need master list of nodes, however.
+
 class Tree(object):
 
-    def __init__(self):
+    def __init__(self, node=None):
+        """ Create a Tree with an optional top node"""
+
         self.clear()
+        if node is not None:
+            self.create(node)
 
 
     def clear(self):
@@ -63,7 +92,21 @@ class Tree(object):
 
 
     def create(self, node):
+        """
+        Creates a tree with the head tree `node`. Will destroy all data
+        currently stored in the tree
+        """
+
         self.clear()
+
+        # if thsese asserts are not True then node must belong to another
+        # tree, and we can't add it to this one
+        assert node.is_leaf() and node.is_root()
+
+        # make sure node is initialized properly
+        node.depth = 1
+        node.parent = None
+
         self.nodes.add(node)
         self.leaves.add(node)
         self.head = node
@@ -82,6 +125,7 @@ class Tree(object):
 
         # add to parent
         parent.children.add(child)
+        child.depth = parent.depth + 1
 
         # add to nodes for easy look up
         self.nodes.add(child)
@@ -95,15 +139,9 @@ class Tree(object):
 
 
 
-
     def delete(self, node):
-        print('delete', node)
-
         # sanity check
         assert node in self.nodes
-
-        if node not in self.nodes:
-            return False
 
         # if I am the root, just delete everything!
         if node.is_root():
@@ -112,6 +150,8 @@ class Tree(object):
             return
 
 
+        # recursively delete children; have to do this to ensure they
+        # are all removed from self.nodes and self.leaves
         for n in node.children:
             self.delete(n)
 
@@ -126,25 +166,6 @@ class Tree(object):
         # parent may have become a new leaf
         if parent.is_leaf():
             self.leaves.add(parent)
-
-
-
-    def branch(self, node):
-        """
-        given a node id, generate a list of all of the kf up to the root,
-        ordered from the head to the specified node
-
-        node_id will typically be a leaf node, but it doesn't have to be.
-        """
-
-        nodes = [node]
-
-        node = node.parent
-        while node is not None:
-            nodes.append(node)
-            node = node.parent
-
-        return list(reversed(nodes))
 
 
     def __len__(self):
@@ -172,8 +193,6 @@ class MultipleHypothesisTracker(object):
 
 
 
-
-
 if __name__ == '__main__':
 
     '''from filterpy.common import kinematic_kf
@@ -193,8 +212,8 @@ if __name__ == '__main__':
     n2 = Node(2)
     t.add_child(n, n2)
 
-    assert len(t.branch(n2)) == 2
-    assert len(t.branch(n)) == 1
+    assert len(n2.branch()) == 2
+    assert len(n.branch()) == 1
 
     assert n.parent == None
     assert n.is_root()
@@ -202,8 +221,8 @@ if __name__ == '__main__':
     assert not n2.is_root()
     assert n2.is_leaf()
 
-    assert n.depth() == 1
-    assert n2.depth() == 2
+    assert n.depth == 1
+    assert n2.depth == 2
 
 
 
