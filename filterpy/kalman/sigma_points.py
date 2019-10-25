Fix computation of the simplex sigma points
The computation of the sigma point was missing a transpose, leading to
incorrect covariance-updates if the covariance was not diagonal.

This error lead to the matrix not being positive semidefinite, and hence
to errors when trying to decompose it using the cholesky algorithm.

filterpy/kalman/sigma_points.py
@@ -500,13 +500,14 @@ class SimplexSigmaPoints(object):
 
         lambda_ = n / (n + 1)
         Istar = np.array([[-1/np.sqrt(2*lambda_), 1/np.sqrt(2*lambda_)]])
+
         for d in range(2, n+1):
             row = np.ones((1, Istar.shape[1] + 1)) * 1. / np.sqrt(lambda_*d*(d + 1))
             row[0, -1] = -d / np.sqrt(lambda_ * d * (d + 1))
             Istar = np.r_[np.c_[Istar, np.zeros((Istar.shape[0]))], row]
 
         I = np.sqrt(n)*Istar
-        scaled_unitary = U.dot(I)
+        scaled_unitary = (U.T).dot(I)
 
         sigmas = self.subtract(x, -scaled_unitary)
         return sigmas.T